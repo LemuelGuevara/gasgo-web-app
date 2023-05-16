ed37112e69653d84963dee76494209df14d50b04
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-05-17 00:26:13 +0800

    Added views for getting stations and single station

--- backend/stations_app/views.py
+++ backend/stations_app/views.py
@@ -8,7 +8,14 @@ from rest_framework import status
 
 # Create your views here.
 class StationList(View):
-    def get(self, request):
+    def get(self, request, format=None):
         stations = Place.objects.all()
         serializer = StationSerializer(stations, many=True)
         return JsonResponse({"stations": serializer.data})
+
+
+class StationDetail(View):
+    def get(self, request, place_id, format=None,):
+        station = Place.objects.get(place_id=place_id)
+        serializer = StationSerializer(station)
+        return JsonResponse({"station": serializer.data})

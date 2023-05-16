4f4729abd13230a5a76562ad04bb18cae668a0b3
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-05-16 19:22:03 +0800

    Installed rest framework and stations app

--- backend/gasgo_rest_api/settings.py
+++ backend/gasgo_rest_api/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'stations_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'gasgo_rest_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'K7GGHwBtHo9ohtNLC02g',
+        'HOST': 'containers-us-west-57.railway.app',
+        'PORT': '6748',
     }
 }
 
--- backend/gasgo_rest_api/urls.py
+++ backend/gasgo_rest_api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stations_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('stations', views.StationList.as_view(), name="stations")
 ]

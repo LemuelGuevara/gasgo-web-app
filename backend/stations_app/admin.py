dbb765d36b1fe81d591a8872e0f3239d8cf0119e
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2023-05-16 17:02:57 +0800

    Regisetered station models

--- backend/stations_app/admin.py
+++ backend/stations_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Location, Viewport, Geometry, Photo, Place
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Viewport)
+admin.site.register(Geometry)
+admin.site.register(Photo)
+admin.site.register(Place)
